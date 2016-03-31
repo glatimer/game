85ff00362c7d39c74f98ed2c369edb02b6b9536c
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-30 19:08:22 -0700

    Attempt to setup constructors for player, space, board, game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 // business logic
+function Player(player1, player2) {
+  this.player1 = player1,
+  this.player2 = player2
+}
+function Space () {
 
+}
 
+function Board (x, y) {
 
+}
 
+function Game () {
+
+}
+
+Player.prototype.mark = function(userInput) {
+  return "X"
+}
 // user interface logic
+$(document).ready(function() {
+  $().submit(function(event) {
+    event.preventDefault();
+  });
+});
